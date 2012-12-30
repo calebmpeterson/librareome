01dab4db0bd733d896c011885c4f41dbdf1e9729
calebmpeterson <caleb.m.peterson@gmail.com>
2012-12-29 21:53:53 -0600

    model updates

--- app/models/Document.java
+++ app/models/Document.java
@@ -1,7 +1,13 @@
 package models;
 
-import javax.persistence.Entity;
+import java.io.IOException;
 
+import javax.persistence.Entity;
+import javax.persistence.PostLoad;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+import play.Logger;
 import play.data.validation.Required;
 import play.db.jpa.Blob;
 import play.db.jpa.Model;
@@ -11,10 +17,10 @@ public class Document extends Model {
 
 	@Required
 	public String title;
-	
+
 	@Required
 	public Blob pdf;
-	
+
 	public int latest = 1;
 
 	public int pages = 1;
@@ -23,6 +29,25 @@ public class Document extends Model {
 		this.title = title;
 		this.pdf = data;
 		this.latest = 1;
+	}
+
+	@PostLoad
+	public void postLoad() {
+		if (this.pages == 0) {
+			Logger.info("correcting 'pages' for %s", this.title);
+			correctPages(this);
+		}
+	}
+
+	public static void correctPages(Document doc) {
+		try {
+			PDDocument pdf = PDDocument.load(doc.pdf.get());
+			doc.pages = pdf.getNumberOfPages();
+
+			doc.save();
+		} catch (IOException e) {
+			Logger.error(e, "error correcting pages for %i", doc.id);
+		}
 	}
 
 }
